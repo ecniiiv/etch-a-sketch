bb93d883d5f8b4f0ff14b7cb1477780886da2440
enciiiv <enciiiv@users.noreply.github.com>
2023-06-02 15:49:12 +0800

    changes display flex to grid

--- main.js
+++ main.js
@@ -1,32 +1,38 @@
 const drawingBoard = document.querySelector("#drawing-board");
 const askGridBtn = document.querySelector("#ask-grid-btn");
 
-// Set up drawing board grid
-
-//  -- ASk for grid size
 let grid = 0;
 askGridBtn.addEventListener("click", () => {
-  drawingBoard.innerHTML = "";
-  grid = Number(prompt("Set up how many grids (max: )"));
-  if (grid > 32) {
-    alert("maximun of 100");
+  grid = prompt("Set the the Grid (Min: 16, Max: 64)");
+  if (grid <= 15 || grid > 100) {
+    alert("Allowed grid is between 16 - 100");
     grid = 0;
-  } else if (grid > 15) {
-    for (let i = 0; i < grid; i++) {
-      for (let j = 0; j < grid; j++) {
-        console.log(i + j);
-
-        drawingBoard.setAttribute(
-          "style",
-          `width: ${grid * grid}px; height: ${grid * grid}px`
-        );
-
-        drawingBoard.innerHTML += `
-        <div class='block' 
-        style="width: ${grid}px; height: ${grid}px">
-        </div> 
-        `;
-      }
-    }
+  } else {
+    renderGrid(grid);
   }
 });
+
+function renderGrid(gridCount) {
+  clearGrid();
+
+  drawingBoard.setAttribute(
+    "style",
+    `grid-template-columns: repeat(${grid}, 1fr);grid-template-rows: repeat(${grid}, 1fr);`
+  );
+
+  let gridOffset = drawingBoard.offsetWidth / grid;
+
+  for (let i = 0; i < gridCount * gridCount; i++) {
+    const div = document.createElement("div");
+    div.setAttribute(
+      "style",
+      `height: ${gridOffset}px; width: ${gridOffset}px`
+    );
+    div.classList.add("block");
+    drawingBoard.appendChild(div);
+  }
+}
+
+function clearGrid() {
+  drawingBoard.innerHTML = "";
+}
