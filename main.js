7c111e23f1dee7345671d3d9f732cb6305e22876
ecniiiv <mrkvnc.a@gmail.com>
2023-06-03 18:51:31 +0800

    added removing color from pallete functionality

--- main.js
+++ main.js
@@ -2,14 +2,21 @@ const drawingBoard = document.querySelector(".drawing-board");
 const gridPickerBtn = document.querySelectorAll(".grid-picker button");
 const colorPickerBtn = document.querySelectorAll("button[data-color]");
 const colorPickerWheel = document.querySelector("input[type='color'");
-const staticColor = document.querySelector(".color-picker .static");
+const staticColors = document.querySelector(".color-picker .static");
 const addButton = document.querySelector(".add-color");
+const removeColorbtn = document.querySelector(".remove-color");
+const rbImage = document.querySelector(".remove-color img");
 
 let gridSize = 16;
 let drawingBoardOffset = drawingBoard.offsetWidth;
 let penColor = "#000000";
 let colorWheelColor = "";
 
+// STATUSES
+let drawMode = false;
+let removeColorMode = false;
+
+// INITIALIZE
 setCanvas();
 
 gridPickerBtn.forEach((btn) => {
@@ -22,24 +29,26 @@ gridPickerBtn.forEach((btn) => {
 
 colorPickerBtn.forEach((btn) => {
   btn.style.backgroundColor = `${btn.dataset.color}`;
-  btn.addEventListener("click", () => setPenColor(btn));
+  btn.addEventListener("click", () => {
+    if (removeColorMode) {
+      staticColors.removeChild(btn);
+    } else {
+      setPenColor(btn);
+    }
+  });
 });
 
 colorPickerWheel.addEventListener("change", () => {
   setPenColorWheel(colorPickerWheel);
 });
 
-addButton.addEventListener("click", () => {
-  if (staticColor.children.length < 12) {
-    const button = document.createElement("button");
-    button.classList.add("saved-color");
-    button.setAttribute("data-color", `${colorWheelColor}`);
-    button.style.backgroundColor = `${button.dataset.color}`;
-    staticColor.appendChild(button);
-
-    button.addEventListener("click", () => setPenColor(button));
+addButton.addEventListener("click", () => addToPallete());
+removeColorbtn.addEventListener("click", () => {
+  removeColorMode = !removeColorMode;
+  if (removeColorMode) {
+    rbImage.setAttribute("src", "./assets/done.svg");
   } else {
-    alert("cannot add more Color to the pallete (limit: 10)");
+    rbImage.setAttribute("src", "./assets/trash.svg");
   }
 });
 
@@ -55,6 +64,7 @@ function setCanvas() {
   for (let i = 0; i < gridSize ** 2; i++) {
     const div = document.createElement("div");
     div.classList.add("disabled-select");
+    div.classList.add("saved-color");
     div.style = `
       height: ${drawingBoardOffset / gridSize}px;
       `;
@@ -63,27 +73,56 @@ function setCanvas() {
   }
 }
 
+// FUNCTIONALITIES
+
 function setPenColor(controller) {
   penColor = controller.dataset.color;
 }
+
 function setPenColorWheel(controller) {
   penColor = controller.value;
   colorWheelColor = controller.value;
 }
 
-// FUNCTIONALITIES
+function addToPallete() {
+  if (staticColors.children.length < 12) {
+    const button = document.createElement("button");
+    button.classList.add("saved-color");
+    button.setAttribute("data-color", `${colorWheelColor}`);
+    button.style.backgroundColor = `${button.dataset.color}`;
+    staticColors.appendChild(button);
 
-let drawMode = false;
+    button.addEventListener("click", () => {
+      if (removeColorMode) {
+        staticColors.removeChild(button);
+        removeColorMode = false;
+        console.log(removeColorMode);
+      } else {
+        setPenColor(button);
+      }
+    });
+  } else {
+    alert("cannot add more Color to the pallete (limit: 10)");
+  }
+}
+
+// function removeToPallete(color) {
+//   if(removeColorMode) {
+//     color.data
+//   }
+// }
+
 window.addEventListener("mousedown", () => (drawMode = true));
 window.addEventListener("mouseup", () => (drawMode = false));
 
 function draw(div) {
+  div.addEventListener("click", () => {
+    div.style.backgroundColor = penColor;
+  });
+
   div.addEventListener("mouseover", () => {
     if (drawMode === true) {
       div.style.backgroundColor = penColor;
     }
-  });
-  div.addEventListener("click", () => {
-    div.style.backgroundColor = penColor;
   });
 }
